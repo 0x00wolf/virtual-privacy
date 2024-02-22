dff0194e9b25b02fbbb7eb231f851cb8a867bddc
Wolf <104569969+0x00wolf@users.noreply.github.com>
2024-02-22 19:34:55 -0400

    Update __init__.py

--- runtime/database/functions/__init__.py
+++ runtime/database/functions/__init__.py
@@ -4,6 +4,7 @@ from runtime.database.functions.show_targets import show_targets
 from runtime.database.functions.show_target import show_target
 from runtime.database.functions.show_tables import show_tables
 from runtime.database.functions.delete_key import delete_key
+from runtime.database.functions.add_target import add_target
 from runtime.database.functions.key_exists import key_exists
 from runtime.database.functions.show_keys import show_keys
 from runtime.database.functions.show_key import show_key
