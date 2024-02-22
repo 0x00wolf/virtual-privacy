64b9266a9d1c853ba391cb2397bb3676fb1e4eee
Wolf <104569969+0x00wolf@users.noreply.github.com>
2024-02-22 19:32:12 -0400

    Update worker.py

--- runtime/database/worker.py
+++ runtime/database/worker.py
@@ -1,97 +1,69 @@
-import runtime.database.functions
-from runtime.database.DatabaseError import DatabaseError
+from Crypto.PublicKey import RSA
+import json
+
+from crypter.CrypterError import CrypterError
+import crypter.chacha20_poly1305
+import crypter.rsa
 
 
-def worker(arguments, database_path):
-    try:
-        # add key
-        if arguments.database == 'add-key':
-            if not arguments.public_key:
-                raise VPRuntimeError(message="Operation requires an RSA public"
-                                             " key to store in the database.")
-            runtime.database.add_key(
-                database_path=database_path,
-                public_key=arguments.public_key,
-                nickname=arguments.target)
+def signed_message(private_key: RSA.RsaKey,
+                   public_key: RSA.RsaKey,
+                   wrapped_key: bytes,
+                   signature: bytes,
+                   ciphertext: str,
+                   verbose: bool = False):
+    """
+       Takes the wrapped key and ciphertext into separate variables. If the
+       path to a PEM key has been provided it will import the key. Another
+       option is to provide an RSA private key that has already imported for
+       decryption.
 
-        # add server
-        elif arguments.database == 'add-server':
-            if not arguments.host or not arguments.port or not arguments.target:
-                raise VPRuntimeError(
-                    message="Operation requires Host, port, and target "
-                            "nickname.\n[ ] Certificate and RSA public key are"
-                            " optional.")
-            print(f"[*] Adding server {arguments.target} to "
-                  f"{arguments.user.title()}'s SQL database: ")
-            runtime.database.add_target(
-                server_nickname=arguments.target,
-                host=arguments.host,
-                port=arguments.port,
-                public_key=arguments.public,
-                certificate=arguments.certificate,
-                database_path=database_path)
+       The 256-bit encrypted session key is unwrapped with the RSA private key
+       and passed to a ChaCha20-Poly1305 stream cipher along with the
+       ciphertext for decryption. The decrypted plaintext is returned as a
+       variable. If verbose=True, detailed information about the decryption
+       process will be provided.
 
-        # show server
-        elif arguments.database == 'show-server':
-            if not arguments.target:
-                raise VPRuntimeError(message="Operation requires a target "
-                                             "ID or nickname.")
-            print(f"[*] Displaying information for {arguments.target} in"
-                  f" {arguments.user.title()}'s SQL database: ")
-            runtime.database.show_target(server_nickname=arguments.target,
-                                         database_path=database_path)
+       Args:
+           wrapped_key (bytes): The RSA wrapped 256-bit decryption key.
+           signature (bytes): The bytes comprising the sender's signature of
+           the message's SHA256 hash.
+           ciphertext (str): ChaCha20 JSON containing the ciphertext.
+           private_key (RSA.RsaKey): The receiver's RSA private key imported
+           for encryption. Either a PEM or an RSA key must be supplied.
+           public_key (RSA.RsaKey): The sender's RSA public key for signature
+           verification.
+           verbose (bool): Print out verbose information about the decryption
+           process.
 
-        # show servers
-        elif arguments.database == 'show-servers':
-            print(f"[*] Displaying all saved servers in "
-                  f"{arguments.user.title()}'s SQL database: ")
-            runtime.database.show_targets(database_path=database_path)
+       Returns:
+           plaintext(str): The decrypted ciphertext message.
 
-        # show key
-        elif arguments.database == 'show-key':
-            if not arguments.target:
-                raise VPRuntimeError(
-                    message="Operation requires a target ID or nickname.")
-            runtime.database.show_key(database_path=database_path,
-                                      target=arguments.target)
+    """
 
-        # show keys
-        elif arguments.database == 'show-keys':
-            print(f"[*] Displaying stored public keys from "
-                  f"{arguments.user.title()}'s SQL database")
-            runtime.database.show_keys(database_path=database_path)
-
-        # delete key
-        elif arguments.database == 'delete-key':
-            if not arguments.target:
-                raise VPRuntimeError(message="Operation requires a target"
-                                             " nickname or ID.")
-            print(f"[*] Deleting key ID {arguments.target} from "
-                  f"{arguments.user.title()}'s SQL database ")
-            runtime.database.delete_key(
-                database_path=database_path,
-                key_id=int(arguments.target))
-
-        # delete server
-        elif arguments.database == 'delete-server':
-            if not arguments.target:
-                raise VPRuntimeError(message="Operation requires a target "
-                                             "nickname or ID.")
-            print(f"[*] Deleting server {arguments.target} from "
-                  f"{arguments.user.title()}'s SQL database: ")
-            runtime.database.delete_target(
-                server_nickname=arguments.target,
-                database_path=database_path)
-
-        # show tables
-        elif arguments.database == 'show-tables':
-            print(f"[*] Displaying table meta data: ")
-            runtime.database.show_tables(database_path)
-
-        print('[*] Database operation successful.')
-
-    except DatabaseError as e:
-        print(f"[!] Database error: {e}")
-
-
+    if verbose:
+        print(f"[ ] Attempting to unwrap session key with local "
+              f"RSA private key."
+              f"\n[ ] First 8 bytes of wrapped key: {wrapped_key[:8]}")
+    unwrapped_key = crypter.rsa.unwrap(private_key=private_key,
+                                       wrapped_key=wrapped_key)
+    if verbose:
+        print(f"[ ] Successfully unwrapped 256-bit session key."
+              f"\n[ ] First 8 bytes of session key: {unwrapped_key[:8]}")
+    if verbose:
+        cipher_t = json.loads(ciphertext)
+        print(f"[ ] Preparing to decrypt ciphertext with session key "
+              f"\n[ ] Ciphertext (32 bytes): {cipher_t['ciphertext'][:32]}")
+    plaintext = crypter.chacha20_poly1305.decrypt(
+        unwrapped_key=unwrapped_key,
+        ciphertext=ciphertext)
+    if verbose:
+        print(f"[*] Successfully decrypted message: {plaintext}")
+    good_signature, message_hash = crypter.rsa.verify(
+        public_key=public_key,
+        signature=signature,
+        bytes_string=plaintext.encode())
+    if verbose:
+        print(f"[*] Signature verified signature with sender's {public_key}")
+    return plaintext
 
